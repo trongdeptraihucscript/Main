e282404ad98d09b9273942e9830a3d1414f9b6ea
TrongNguyen <trongnguyenuub129@gmail.com>
2025-11-20 12:57:21 +0700

    Add files via upload

--- TN-Tp-Chest.lua
+++ TN-Tp-Chest.lua
@@ -1,7 +1,8 @@
 --  TrongNguyen Hub
 -- deobf = ba mẹ mất sớm
+-- ko deobf nha
 local Scripts = {
-    [994732206] = "https://raw.githubusercontent.com/trongdeptraihucscript/Trdz-hub/refs/heads/main/41613vZS96n3550D21XZ707nM2Xr4HrFnT51n9U25H3N3z3m1XqIXc71h3D146tU.lua.txt",
+    [994732206] = "https://raw.githubusercontent.com/trongdeptraihucscript/Trdz-hub/refs/heads/main/obfuscated_script-1763617703227.lua.txt",
 }
 
 local url = Scripts[game.GameId]
