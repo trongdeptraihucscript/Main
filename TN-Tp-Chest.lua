217114daa22a0759b09acc2c91b4360147f6763d
TrongNguyen <trongnguyenuub129@gmail.com>
2025-12-13 20:52:08 +0700

    Add files via upload

--- TN-Tp-Chest.lua
+++ TN-Tp-Chest.lua
@@ -1,2 +1,2 @@
 -- deobf = mai ba mẹ chết
-loadstring(game:HttpGet("https://raw.githubusercontent.com/trongdeptraihucscript/Main/refs/heads/main/obfuscated_script-1765130398915.lua.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/trongdeptraihucscript/Main/refs/heads/main/obfuscated_script-1765633791047.lua.txt"))()
