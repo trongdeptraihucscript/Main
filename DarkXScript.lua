70d41ca5234e581c8f1c3245aa598b9609b0aec8
TrongNguyen <trongnguyenuub129@gmail.com>
2026-03-30 22:55:48 +0700

    Add files via upload

--- DarkXScript.lua
+++ DarkXScript.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/trongdeptraihucscript/Source-obf/refs/heads/main/2140096482489532.lua.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/trongdeptraihucscript/Source-obf/refs/heads/main/2598588248074779.lua.txt"))()
